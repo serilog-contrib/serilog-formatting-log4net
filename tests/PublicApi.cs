0caf99931188c62e3c6e16f57450c659fa388bcb
Cédric Luthi <cedric.luthi@gmail.com>
2021-02-23 08:49:55 +0100

    Add public API approval test

---
+++ tests/PublicApi.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using System.Reflection;
+using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using PublicApiGenerator;
+using Xunit;
+
+namespace Serilog.Formatting.Log4Net.Tests
+{
+    public class PublicApi
+    {
+        [Fact]
+        public void ApprovePublicApi()
+        {
+            var assembly = typeof(Log4NetTextFormatter).Assembly;
+            var publicApi = assembly.GeneratePublicApi();
+            var writer = new ApprovalTextWriter(publicApi);
+            Approvals.Verify(writer, new AssemblyNamer(assembly), new DiffReporter());
+        }
+
+        private class AssemblyNamer : UnitTestFrameworkNamer
+        {
+            private readonly Assembly _assembly;
+
+            public AssemblyNamer(Assembly assembly) => _assembly = assembly;
+
+            public override string Name => nameof(PublicApi) + "." + Path.GetFileNameWithoutExtension(_assembly.Location);
+        }
+    }
+}

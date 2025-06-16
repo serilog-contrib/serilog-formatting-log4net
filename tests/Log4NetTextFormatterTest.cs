d0b993ed38fe81b6867c24e65ea5baba5732fac3
Cédric Luthi <cedric.luthi@gmail.com>
2025-06-16 10:57:08 +0200

    Update test dependencies

--- tests/Log4NetTextFormatterTest.cs
+++ tests/Log4NetTextFormatterTest.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Serilog.Core;
 using Serilog.Events;
 using Serilog.Parsing;

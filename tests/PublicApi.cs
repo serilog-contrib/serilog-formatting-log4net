fecadbc19f4522189066734f190790ee23be3efb
Cédric Luthi <cedric.luthi@gmail.com>
2026-01-18 19:13:24 +0100

    Use the artifacts output layout
    Also remove all conditions on ContinuousIntegrationBuild in the tests and store test results and coverage files in the artifacts directory.

--- tests/PublicApi.cs
+++ tests/PublicApi.cs
@@ -17,21 +17,21 @@ public class PublicApi
     public Task ApprovePublicApi(string targetFramework)
     {
         var testAssembly = typeof(PublicApi).Assembly;
-        var configuration = testAssembly.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration
+        var configuration = testAssembly.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration.ToLowerInvariant()
                             ?? throw new InvalidDataException($"{nameof(AssemblyConfigurationAttribute)} not found in {testAssembly.Location}");
-        var assemblyPath = Path.Combine(GetSrcDirectoryPath(), "bin", configuration, targetFramework, "Serilog.Formatting.Log4Net.dll");
+        var assemblyPath = Path.Combine(GetRootDirectoryPath(), "artifacts", "bin", "Serilog.Formatting.Log4Net", $"{configuration}_{targetFramework}", "Serilog.Formatting.Log4Net.dll");
         var assembly = Assembly.LoadFile(assemblyPath);
         var publicApi = assembly.GeneratePublicApi();
         return Verifier.Verify(publicApi, "cs").UseFileName($"PublicApi.{targetFramework}");
     }
 
-    private static string GetSrcDirectoryPath([CallerFilePath] string path = "") => Path.Combine(Path.GetDirectoryName(path)!, "..", "src");
+    private static string GetRootDirectoryPath([CallerFilePath] string path = "") => Path.Combine(Path.GetDirectoryName(path)!, "..");
 
     private class TargetFrameworksTheoryData : TheoryData<string>
     {
         public TargetFrameworksTheoryData()
         {
-            var csprojPath = Path.Combine(GetSrcDirectoryPath(), "Serilog.Formatting.Log4Net.csproj");
+            var csprojPath = Path.Combine(GetRootDirectoryPath(), "src", "Serilog.Formatting.Log4Net.csproj");
             var project = XDocument.Load(csprojPath);
             var targetFrameworks = project.XPathSelectElement("/Project/PropertyGroup/TargetFrameworks")
                                    ?? throw new InvalidDataException($"TargetFrameworks element not found in {csprojPath}");
